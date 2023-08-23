7f3f1b0852f616ebc3cc93120d7d63240f409f62
H595487 <70063744+H595487@users.noreply.github.com>
2023-08-23 23:52:40 +0200

    Update script.js
    Lagt til noen sikkerhetsfunksjoner og beskrivelse

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Dette grensesnittet er designet for effektiv interaksjon med Universal Robots UR5e.
+// Koden sikrer rask og stabil kommunikasjon, minimerer latens og gir klare tilbakemeldinger
+// til brukeren under robotoperasjoner.
+
+// Mekanisme for tilbakemelding: For å gi brukeren kontinuerlig informasjon om robotens tilstand, spesielt under lengre operasjoner.
+function showFeedback(message) {
+    const feedbackElement = document.getElementById('feedback');
+    feedbackElement.textContent = message;
+    setTimeout(() => feedbackElement.textContent = '', 5000); // fjerner meldingen etter 5 sekunder
+}
 document.addEventListener("DOMContentLoaded", function() {
     // Elements
     const connectBtn = document.getElementById("connect-btn");
@@ -10,6 +20,14 @@ document.addEventListener("DOMContentLoaded", function() {
     const hindringerLog = document.getElementById("hindringer-log");
     const scanSummary = document.getElementById("scan-summary");
 
+    // Buffer for DOM-elementer for å redusere gjentatte oppslag og forbedre ytelsen.
+    const domCache = {
+        robotStatus: document.getElementById('robot-status'),
+        // ... flere cache-elementer kan legges til her
+    };
+    function isValidCommand(command) {
+        return ['startScanMode', 'endScanMode', 'moveForward', 'moveBackward'].includes(command);
+    }
     connectBtn.addEventListener("click", function() {
         robotStatus.textContent = "Tilkoblet";
         batteryLevel.textContent = "80%";
@@ -45,7 +63,15 @@ document.addEventListener("DOMContentLoaded", function() {
         alert("NØDSTOPP aktivert! Robot stopper umiddelbart.");
         // Her kan vi legge til faktisk kode for å sende en nødstopp-kommando til roboten i en virkelig implementasjon.
     });
+    // Modul for fremtidige robot-kommandoer
+    const RobotCommands = {
+        moveForward: function() {
+            // logikk for å bevege roboten fremover
+        },
+        moveBackward: function() {
+            // logikk for å bevege roboten bakover
+        }
+        // ... flere kommandoer kan legges til her
+    };
 
 });
-
-
